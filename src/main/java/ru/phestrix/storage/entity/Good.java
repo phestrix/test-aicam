faac71b02ad0ddac22e6a828dab6acda2cd4c3a5
matri <matri@NITRO5>
2023-10-05 23:58:54 +0700

    refactor to repository

---
+++ src/main/java/ru/phestrix/storage/entity/Good.java
@@ -0,0 +1,17 @@
+package ru.phestrix.storage.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Good {
+    private Integer id;
+    private String name;
+    private Integer price;
+}
---
+++ src/main/java/ru/phestrix/storage/repositories/GoodRepository.java
@@ -0,0 +1,78 @@
+package ru.phestrix.storage.repositories;
+
+import ru.phestrix.storage.databaseConnection.DatabaseConnection;
+import ru.phestrix.storage.entity.Good;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class GoodRepository {
+    private Connection connection = DatabaseConnection.getConnection();
+
+    public void update(Good good) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "update good set name = ?, price = ? where id = ?"
+            );
+            statement.setString(1, good.getName());
+            statement.setInt(2, good.getPrice());
+            statement.setInt(3, good.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(Good good) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into good (name, price) VALUES (?,?)"
+            );
+            statement.setString(1, good.getName());
+            statement.setInt(2, good.getPrice());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Optional<Good> getGoodById(Integer id) {
+        Optional<Good> good = Optional.of(new Good());
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "select * from good where id = ?"
+            );
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+
+            good.ifPresent(it -> {
+                try {
+                    it.setId(result.getInt("id"));
+                    it.setName(result.getString("name"));
+                    it.setPrice(result.getInt("price"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            result.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return good;
+    }
+
+    public void delete(Good good) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "delete from good where id = ?"
+            );
+            statement.setInt(1, good.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
