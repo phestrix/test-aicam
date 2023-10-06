53f862f4bfc32a019634b56ce887f9ef51c8c2db
matri <matri@NITRO5>
2023-10-07 01:39:14 +0700

    deleted

--- src/main/java/ru/phestrix/exceptions/BadNumberOfArgsException.java
+++
@@ -1,7 +0,0 @@
-package ru.phestrix.exceptions;
-
-public class BadNumberOfArgsException extends RuntimeException{
-    public BadNumberOfArgsException(String message){
-        super(message);
-    }
-}
