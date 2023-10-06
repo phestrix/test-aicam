8e9eb02c2fe92c2d05a59377f2e37adb8d429625
matri <matri@NITRO5>
2023-10-07 01:40:20 +0700

    add static method to parse command line args

--- src/main/java/ru/phestrix/commandLineParser/CommandLineParser.java
+++ src/main/java/ru/phestrix/commandLineParser/CommandLineParser.java
@@ -1,9 +1,23 @@
 package ru.phestrix.commandLineParser;
 
-public class CommandLineParser {
-    public void parseArgs(String[] args){
-        if(args.length < 3){
+import java.util.Objects;
 
+public class CommandLineParser {
+
+    public static String parseArgs(String[] args) {
+        if (args.length != 3) {
+            return "Bad number of arguments";
         }
+        String nameOfOperation = args[0];
+        String pathToInputFile = args[1];
+        String pathToOutputFile = args[2];
+        if (!Objects.equals(nameOfOperation, "search") ||
+                !Objects.equals(nameOfOperation, "stat")) {
+            return "unsupported type of operation";
+        }
+        if (!pathToInputFile.contains(".json") || !pathToOutputFile.contains(".json")) {
+            return "unsupported file format";
+        }
+        return nameOfOperation;
     }
 }
