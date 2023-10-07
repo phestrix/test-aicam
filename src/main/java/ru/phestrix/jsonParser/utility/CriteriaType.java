2cbc5614bdfc0f3826c144ae1c3d16d8b4d451db
matri <matri@NITRO5>
2023-10-07 18:15:03 +0700

    types to parse

---
+++ src/main/java/ru/phestrix/jsonParser/utility/CriteriaType.java
@@ -0,0 +1,12 @@
+package ru.phestrix.jsonParser.utility;
+
+public enum CriteriaType {
+    LASTNAME,
+    MIN_MAX_EXPENSES,
+    BAD_CUSTOMERS,
+    PRODUCT_COUNT,
+    DATES,
+    UNSUPPORTED_SINGLE_TYPE,
+    UNSUPPORTED_DOUBLE_TYPE,
+    UNSUPPORTED_CRITERIA;
+}
